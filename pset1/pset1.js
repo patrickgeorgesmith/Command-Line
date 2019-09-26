106fab54f28e4a3d710e2fd24c4348cbad7128ac
patrickgeorgesmith <patricksmith@pursuit.org>
2019-09-26 19:33:13 -0400

    Command Line

--- pset1/pset1.js
+++ pset1/pset1.js
@@ -4,50 +4,72 @@ PROBLEM 1:
 Follow the following steps ONLY using your command line:
 
 1. Navigate to your Desktop directory.
-  cd 
-  ls 
-  cd Desktop
+    ```  
+    cd 
+    ls 
+    cd Desktop
+    ```
 
 2. Create a directory called my_first_direc.
-  cd Desktop
-  mkdir my_first_direc
+    ```
+    cd Desktop
+    mkdir my_first_direc
+    ```
 
 3. Enter the my_first_direc directory.
+    ```
     cd Desktop
     cd my_first_direc
+    ```
 
 4. Create a file called first.js.
-   cd my_first_direc 
-   touch first.js
+    ```
+    cd my_first_direc 
+    touch first.js
+    ```
 
 5. Create a folder called assignments.
-   cd Desktop
-   ls
-   cd my_first_direc
-   mkdir assignments
- 
-6. Move first.js into assignments.
-    mv first.js assignments
+    ```
+    cd Desktop
+    ls
+    cd my_first_direc
+    mkdir assignments
+    ```
 
+6. Move first.js into assignments.
+    ```
+    mv first.js assignments
+    ```
 7. Go into the assignments folder.
+    ```    
     cd assignments
+    ```
 
 8. Delete first.js.
-   rm first.js  
+    ```
+    rm first.js  
+    ```
 
 9. Go up to the my_first_direc directory.
+    ```    
     cd..
+    ```
 
 10. Delete the assignments folder.
+    ```
     cd my_first_direc directory
     rm -r assignments
+    ```
 
 11. Go up to the Desktop directory.
+    ```    
     cd ..
+    ```
 
 12. Delete the test folder.
+    ```    
     rm -r my_first_direc
-
+    ```
 ******************/
 
 
@@ -58,29 +80,39 @@ PROBLEM 2:
 Follow the following steps ONLY using your command line:
 
 1. Navigate to the inside of your Documents folder
+    ```    
     cd Documents
+    ```
 
 3. Create a file called foo.js
+    ```
     touch foo.js
-
+    ```
 4. Open the file with a text editor, (Atom/VSCode)
+    ```
     code foo.js
-
+    ```
 5. Add the following text to the file: console.log('hello')
+    ```
     console.log('hello')
-
+    ```
 6. Save and exit the file
+    ```
     Command S
-
+    ```
 7. Open the file again with a text editor, (Atom/VSCode)
+    ```
     code foo.js
-
+    ```
 8. Add the following in a new line: hellohellohello
+    ```
     hellohellohello
-
+    ```
 9. Exit without saving
+    ```
     Command Q
-******************/
+    ```
+    ******************/
 
 
 
